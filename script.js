abbb4340c99db372820dfc849531d6ee3c4a5719
Maurice Andreas <36079435+BorrisMitM@users.noreply.github.com>
2020-05-22 19:21:34 +0200

    added accordions

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-function submitAnswers(){
-  var score = 0;
-  var answers = [1, 0, 2, 2];
 
-  var name = document.querySelector('input[name="name"]').value;
-  var f1 = document.querySelector('input[name="f1"]:checked').value;
-  var f2 = document.querySelector('input[name="f2"]:checked').value;
-  var f3 = document.querySelector('input[name="f3"]:checked').value;
-  var f4 = document.querySelector('input[name="f4"]:checked').value;
-
-}
+$( function() {
+  $( "#accordion" ).accordion({
+    collapsible: true
+  });
+} );
